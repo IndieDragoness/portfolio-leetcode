156bcd47b45a752f50471bfb370248ebfa64d07f
Sage <indiedragoness@protonmail.com>
2021-05-16 18:02:54 -0500

    Update Trapping Water

--- Array_Problems/trapping_rain_water.py
+++ Array_Problems/trapping_rain_water.py
@@ -9,20 +9,25 @@
 #   - list[start : end : step]
 #   - If no end is specified, it will keep stepping through til end
 
-#              _   
-#      _      | |_   _
-#  _  | |_   _|   |_| |_
-# | |_|   |_|           |
-elevation_array = [0,1,0,2,1,0,1,3,2,1,2,1]
+def calculate_trapped_water(elevation_array):
+    water_volume = 0
 
-water_volume = 0
+    for i, current_height in enumerate(elevation_array):
+        max_right = max(elevation_array[i::1])
+        max_left = max(elevation_array[i::-1])
 
-for i, current_height in enumerate(elevation_array):
-    max_right = max(elevation_array[i::1])
-    max_left = max(elevation_array[i::-1])
+        if min(max_right, max_left) > current_height:
+            water_volume += min(max_right, max_left) - current_height
 
-    if min(max_right, max_left) > current_height:
-        water_volume += min(max_right, max_left) - current_height
+    return water_volume
 
-# Print the calculated water volume
-print(water_volume)
+if __name__ == "__main__":
+    # W: Where water should exist.
+    #              _   
+    #      _      | |_   _
+    #  _  | |W W W|   |W| |_
+    # | |W|   |W|           |
+    elevation_array = [0,1,0,2,1,0,1,3,2,1,2,1]
+
+    water_volume = calculate_trapped_water(elevation_array)
+    print("Calculated water Volume: {}".format(water_volume))
